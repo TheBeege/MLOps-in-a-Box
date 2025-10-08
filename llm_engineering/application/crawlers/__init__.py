ac541967381db34537fdaa288682e5a6b83307dc
Bryan "Beege" Berry <draco4king@gmail.com>
2025-10-08 15:39:12 +0900

    feat: Replaced their data with my Medium articles.

--- llm_engineering/application/crawlers/__init__.py
+++ llm_engineering/application/crawlers/__init__.py
@@ -1,6 +1,5 @@
 from .dispatcher import CrawlerDispatcher
 from .github import GithubCrawler
-from .linkedin import LinkedInCrawler
 from .medium import MediumCrawler
 
-__all__ = ["CrawlerDispatcher", "GithubCrawler", "LinkedInCrawler", "MediumCrawler"]
+__all__ = ["CrawlerDispatcher", "GithubCrawler", "MediumCrawler"]
